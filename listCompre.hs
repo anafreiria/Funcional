c8cfe507fd11875d7f8302b3d426f7cbc1d0031e
anafreiria <ana.freiria@sou.unifal-mg.edu.br>
2025-05-14 21:14:32 -0300

    Atualizações

--- listCompre.hs
+++ listCompre.hs
@@ -20,7 +20,7 @@ f3_normal x (a:b)
 f3 :: Int -> [Int] -> [Int]
 f3 x l = [x*a | a <- l, (mod) a 2 == 0] ++ [a | a <- l, (mod) a 2 /= 0]
 
--- Ideia vista baseada em livro, deve ser testada (não sei se funciona)
+-- Ideia vista em livro, deve ser testada
 f3_teste :: Int -> [Int] -> [Int]
 f3_teste x l = [if (mod) a 2 == 0 then (x * a) else a | a <- l] 
 
@@ -36,3 +36,45 @@ f5 x l = [a * x | a <- l, (mod) a 2 == 0 && a > 5]
 -- lista com elementos * x pares ou menores que 5
 f6 :: Int -> [Int] -> [Int]
 f6 x l = [a * x | a <- l, (mod) a 2 == 0 || a < 5]
+
+
+
+-- uso do $$
+--------------------------------
+--zip [1,2,3] ["a","bb", "ccc"]
+--unzip $$
+--------------------------------
+-- 3+5
+-- 32 - $$
+
+{- função que multiplica por x cada elemento de uma lista -}
+f1_e x l = [a*x| a<-l]
+
+{- função que filtra os pares e os multiplica por x-}
+f2_e x l = [a*x| a<-l, (mod) a 2 == 0]
+
+{- função que multiplica por x apenas os números pares de uma lista -}
+f3_e x l = [a*x| a<-l, (mod) a 2 == 0] ++ [a| a<-l, (mod) a 2 /= 0]  
+
+{- função que filtra os pares maiores que 5 e os multiplica por x- -}
+f4_e x l = [a*x| a<-l, (mod) a 2 == 0, a > 5]  
+
+{- função que filtra os pares ou maiores que 5 e os multiplica por x- -}
+f5_e x l = [a*x| a<-l, (mod) a 2 == 0 || a > 5]  
+
+{- função que faz um produto cartesiano entre duas listas
+   sendo uma dada como parâmetro e outra gerada dentro da função-}
+
+produtoCartesiano :: [a] -> [(a, Int)]
+produtoCartesiano xs = [(x, y) | x <- xs, y <- [1..4]]
+
+
+
+
+
+
+
+
+
+
+
